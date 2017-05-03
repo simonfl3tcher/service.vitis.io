64e4517721df76d62cca01243cd6b3b4b6efdf68
Simon Fletcher <simonfletcher0@gmail.com>
2017-05-03 09:08:52 +0100

    mongodb

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 gem 'sinatra', '1.1.0'
 gem 'dotenv'
+gem 'mongo', '~> 2.2', '>= 2.2.5'
+gem 'bson_ext'
--- web.rb
+++ web.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'dotenv/load'
+require 'mongo'
+require 'json/ext' # required for .to_json
+
+configure do
+  db = Mongo::Client.new([ ENV['MONGODB_URI'] ], :database => 'vitis')
+  set :mongo_db, db[:vitis]
+end
 
 ### Routes ###
 get '/' do
   'Hello Simon Fletcher!'
+end
+
+get '/collections/?' do
+  content_type :json
+  settings.mongo_db.database.collection_names.to_json
 end
